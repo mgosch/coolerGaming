6dc06252a06e5a86d41b7a9dc1869f86f945a4a9
Victoria <Victoria@LAPTOP-GLBOKJMD>
2019-10-13 16:24:08 -0300

    Se agrega metodo de registrarPadre

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,17 @@
+import java.text.SimpleDateFormat;
 
 public class Usuario {
 
+	public UsuarioRegistrado registrarPadre(String nombre, String apellido, String usuario, String contraseña, 
+			String fechaNacimiento, String mail, int telefono) throws Exception {
+		UsuarioRegistrado usuarioRegistrado = new UsuarioRegistrado();
+		usuarioRegistrado.setNombre(nombre);
+		usuarioRegistrado.setApellido(apellido);
+		usuarioRegistrado.setUsuario(usuario);
+		usuarioRegistrado.setContraseña(contraseña);
+		usuarioRegistrado.setFechaNacimiento(new SimpleDateFormat("dd/MM/yyyy").parse(fechaNacimiento));
+		usuarioRegistrado.setMail(mail);
+		usuarioRegistrado.setTelefono(telefono);
+	 return usuarioRegistrado;
+	}
 }
