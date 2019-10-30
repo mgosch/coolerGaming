6fa5044a1c8ec525a5a0182db9aacb5763b72e94
Puesto_49 <Puesto_49@NB102.mshome.net>
2019-10-30 19:07:46 -0300

    Refactor

--- src/test/TestRegistracion.java
+++ src/test/TestRegistracion.java
@@ -3,7 +3,7 @@ package test;
 import coolerGaming.Usuario;
 import coolerGaming.UsuarioRegistrado;
 
-public class testRegistracion {
+public class TestRegistracion {
     public static void main(String[] args) throws Exception {
     	Usuario u = new Usuario();
     	UsuarioRegistrado ur1 = u.registrarse("Jose", "Perez", "usuario", "contrasena2", "10/12/1978", "m@gmail.com", 4632-4260);
